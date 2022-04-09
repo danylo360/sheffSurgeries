af48715d22cbd33656dbaad2e6698c7ad428079d
danylo360 <c0010969@my.shu.ac.uk>
2022-04-09 13:51:19 +0000

    domain controllers

--- appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
+++ appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
@@ -2,16 +2,8 @@ package appointmentsystem
 
 class BootStrap {
 
-    def init = { servletContext ->
- //Define objects
-Appointment
-Doctor
-Nurse
-Patient
-Prescription
-Receptionist
-Surgery
-    }
+
+ 
     def destroy = {
     }
 }
